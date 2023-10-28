57618b1ea9316dd15209d1af42bed6ea96be365e
AlexBaghdasaryan <alex.baghdasaryan@list.ru>
2023-10-28 18:54:35 +0400

    init

---
+++ MT340/MT340.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	double s = 0, x = 1;
+	int n, i;
+
+	do
+	{
+		cout << "n="; cin >> n;
+	} 
+	while (n <= 1);
+
+	for (i = 1; i <= n; i++) 
+	{
+		s += x * x;
+		x = 0.5 * fabs(x - 4);
+	}
+
+	cout << "S=" << s;
+
+	return 0;
+}
